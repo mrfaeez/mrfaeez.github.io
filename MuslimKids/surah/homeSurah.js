59814c203c9757221e76701858e6782af5db7019
fasa79 <48919366+fasa79@users.noreply.github.com>
2019-04-27 15:40:38 +0800

    complete and accept improvement

--- MuslimKids/surah/homeSurah.js
+++ MuslimKids/surah/homeSurah.js
@@ -1,5 +1,6 @@
 
 var client = new XMLHttpRequest();
+
 client.open("GET", "http://api.alquran.cloud/v1/surah", true);
 client.onreadystatechange = function() {
     if(client.readyState == 4) {
@@ -14,32 +15,69 @@ client.onreadystatechange = function() {
 
 client.send();
 
+//MODAL VARIABLEs
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
 
-
-
-
-
-
-$(".display").click(function(){
-
-    $('.quran').empty();
+$(".quran").click(function(){
+    
+    $('.modal-content h1').empty();
+    $('.modal-content p').empty();
+    $('.loader').show();
+    $(".inside p").css("direction", "rtl");
+    
+    modal.style.display = "block";
     var noSurah = document.getElementById("option").value;
     
     client.open("GET", "http://api.alquran.cloud/v1/surah/" + noSurah, true);
     client.onreadystatechange = function() {
         if(client.readyState == 4) {
             var obj = JSON.parse(client.responseText);
-            
-            $('.quran').append("<p>");
+            $('.loader').hide();
 
+            $('.modal-content h1').append(obj['data']['name']);
             for (var x = 0 ; x < Object.keys(obj['data']['ayahs']).length; x++){
-                $(".quran").append(obj['data']['ayahs'][x]['text'] + " " + obj['data']['ayahs'][x]['numberInSurah']);
+                $(".modal-content p").append(obj['data']['ayahs'][x]['text'] + "  (" + obj['data']['ayahs'][x]['numberInSurah'] + ")  ");
             }
-            
-            $('.quran').append("</p>")
             
         };
     };
     client.send();
     
-  });
+});
+
+$(".translate").click(function(){
+    
+    $('.modal-content h1').empty();
+    $('.modal-content p').empty();
+    $('.loader').show();
+    $(".inside p").css("direction", "ltr");
+    
+    modal.style.display = "block";
+    var noSurah = document.getElementById("option").value;
+    
+    client.open("GET", "http://api.alquran.cloud/v1/surah/" + noSurah + "/en.asad", true);
+    client.onreadystatechange = function() {
+        if(client.readyState == 4) {
+            var obj = JSON.parse(client.responseText);
+            $('.loader').hide();
+
+            $('.modal-content h1').append(obj['data']['name']);
+            for (var x = 0 ; x < Object.keys(obj['data']['ayahs']).length; x++){
+                $(".modal-content p").append(obj['data']['ayahs'][x]['text'] + "  (" + obj['data']['ayahs'][x]['numberInSurah'] + ")  ");
+            }    
+        };
+    };
+    client.send();
+    
+});
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
