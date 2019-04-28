fb4951aad786fa4e4242705e3fd722a02426ba62
fasa79 <48919366+fasa79@users.noreply.github.com>
2019-04-28 17:08:11 +0800

    testCubeTransition

---
+++ MuslimKids/surah/cube.js
@@ -0,0 +1,15 @@
+
+
+function autocheck(){
+        $('input')
+           .eq( ( $('input:checked').index() + 1 ) % 6 )
+           .prop( 'checked', true );
+}
+
+var interval = setInterval(autocheck, 3000);
+
+$('.three-d-item').hover(function(ev){
+    clearInterval(interval);
+}, function(ev){
+    interval = setInterval( autocheck, 3000);
+});
